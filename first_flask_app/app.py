4e38e12979c078db99ce2fefaa142524ab09fcda
Deepansh Pandey <easypandey.dp@gmail.com>
2024-10-05 13:56:30 +0200

    "JWT Token Added"

--- first_flask_app/app.py
+++ first_flask_app/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_migrate import Migrate
 from flask_smorest import Api
+from flask_jwt_extended import JWTManager
 
 from db import db
 
@@ -8,7 +9,8 @@ import models
 
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
-
+from resources.tag import blp as TagBlueprint
+from resources.user import blp as UserBlueprint
 
 def create_app(db_url=None):
     app = Flask(__name__)
@@ -27,10 +29,15 @@ def create_app(db_url=None):
     migrate = Migrate(app, db)
     api = Api(app)
 
+    app.config["JWT_SECRET_KEY"] = "jose"
+    jwt = JWTManager(app)
+    
     with app.app_context():
         db.create_all()
 
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(StoreBlueprint)
+    api.register_blueprint(TagBlueprint)
+    api.register_blueprint(UserBlueprint)
 
     return app
